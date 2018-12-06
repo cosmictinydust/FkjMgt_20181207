356da02dab5f5d7312b2f75692cd7d59328a1f9e
随意 <cosmictinydust@hotmail.com>
2018-12-07 00:42:39 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FkjMgt_20181207.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
