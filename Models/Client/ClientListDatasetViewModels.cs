975a33fb46b3a155f3e7085f3ef381f1691b2f0f
cosmictinydust <cosmictinydust@hotmail.com>
2018-12-20 17:37:28 +0800

    2018-12-20

--- Models/Client/ClientListDatasetViewModels.cs
+++ Models/Client/ClientListDatasetViewModels.cs
@@ -13,14 +13,25 @@ namespace FkjMgt_20181207.Models.Client
         public string DeparID { get; set; }
         [BindProperty]
         public string EmpID { get; set; }
+        [BindProperty]
+        public string CurrentMonth { get; set; }
         public IEnumerable<SelectListItem> DeparLists { get; set; }
         public IEnumerable<SelectListItem> EmpLists { get; set; }
+        public List<SelectListItem> YearMonth { get; set; }
         public IEnumerable<ClientListDetail> clientListDetails { get; set; }
         public IEnumerable<ClientListTotal> clientListTotals { get; set; }
 
         public ClientListDatasetViewModels(Data.ApplicationDbContext context)
         {
             DeparLists = MyClass.PopulateSomethingsToList.PopulateDeparList(context);
+            string TobeYM = "";
+            //for (int i=0;i>=-2;i--)
+            //{
+            //    TobeYM = DateTime.Now.AddMonths(i).ToString("YYYYMM");
+            //    YearMonth.Add(new SelectListItem { Value = TobeYM, Text = TobeYM });
+            //}
+            var Tobe = new SelectListItem { Value = "0", Text = "abc" };
+            YearMonth.Add(Tobe);
         }
     }
 }
